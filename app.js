352568da6aaef6a79a5a6ce088f6c1298aec3806
Oksana Dmytruk <dmytruk.oksi@gmail.com>
2016-06-03 10:28:01 +0300

    changes

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ var bodyParser = require('body-parser');
 // New Code
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL + db_name);
+}
+
+mongoose.connect('mongodb://127.0.0.1:27017/' + db_name);
 
 var user = require('./models/users');
 
@@ -19,8 +24,11 @@ var routes = require('./routes/index');
 
 var app = express();
 
-app.listen('3000', function () {
-  console.log('App listening on port 3001!');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
 });
 
 // view engine setup
